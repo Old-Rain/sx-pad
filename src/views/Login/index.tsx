b36e0d03091378d37e83a1f83b95d3f3f8209bd0
Old-rain <qlbj949928210@163.com>
2020-09-16 23:49:14 +0800

    虚拟登录

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { FC, PropsWithChildren } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
 
 import logo from '@/assets/logo.png'
 import styles from './index.module.scss'
 
-interface LoginProps {}
+interface LoginProps extends RouteComponentProps {}
 
 const Login: FC<LoginProps> = (props: PropsWithChildren<LoginProps>) => {
   return (
@@ -14,7 +15,9 @@ const Login: FC<LoginProps> = (props: PropsWithChildren<LoginProps>) => {
           <img src={logo} alt="" />
         </h1>
 
-        <div className={styles.form}></div>
+        <div className={styles.form}>
+          <button onClick={() => props.history.push('/home')}>login</button>
+        </div>
       </div>
     </div>
   )

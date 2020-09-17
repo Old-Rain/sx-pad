cf342d3a6b16b9eec65534b4d4dec2d9ab6e3752
unknown <qlbj949928210@163.com>
2020-09-18 00:43:55 +0800

    完成qrcode基本API使用

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { FC, PropsWithChildren } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
+
+import QRCode from 'qrcode'
 
 import logo from '@/assets/logo.png'
 import styles from './index.module.scss'
@@ -8,6 +10,22 @@ import styles from './index.module.scss'
 interface LoginProps extends RouteComponentProps {}
 
 const Login: FC<LoginProps> = (props: PropsWithChildren<LoginProps>) => {
+  const canvas = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    QRCode.toCanvas(
+      canvas.current,
+      'sample text',
+      {
+        width: 300,
+      },
+      function (error) {
+        if (error) console.error(error)
+        console.log('success!')
+      },
+    )
+  })
+
   return (
     <div className={styles.Login}>
       <div className={styles.board}>
@@ -17,6 +35,7 @@ const Login: FC<LoginProps> = (props: PropsWithChildren<LoginProps>) => {
 
         <div className={styles.form}>
           <button onClick={() => props.history.push('/home')}>login</button>
+          <canvas ref={canvas} className={styles.canvas}></canvas>
         </div>
       </div>
     </div>

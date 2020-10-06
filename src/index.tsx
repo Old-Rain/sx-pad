36cceba7a0c81669c1c3dda845400308aa8460fa
Old-rain <qlbj949928210@163.com>
2020-10-06 19:54:44 +0800

    使用memo对部分组件进行优化

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ import './styles/index.scss'
 import App from './zApp/App'
 import * as serviceWorker from './serviceWorker'
 
-
-console.log('东风吹散梁溪晚，运河不禁向北流');
-
+console.log('东风吹散梁溪晚，运河不禁向北流')
 
 ReactDOM.render(
   // <React.StrictMode>

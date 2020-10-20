31f60ef483b482bd5e3240e9abf84081e6a83563
Old-Rain <qlbj949928210@163.com>
2020-10-20 19:31:22 +0800

    初步添加react-redux

--- src/zApp/App.tsx
+++ src/zApp/App.tsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import store from '@/store'
 import { getToken } from '@/utils/userInfo'
 
 import { ConfigProvider } from 'antd'
@@ -18,29 +20,31 @@ import locale from 'antd/es/locale/zh_CN'
 
 function App() {
   return (
-    <ConfigProvider locale={locale}>
-      <Router>
-        <Switch>
-          <Route path="/" exact render={() => <Redirect to={getToken() ? '/home' : '/login'} />} />
-          <Route path="/login" component={Login} />
-          <>
-            <div className={styles.layout}>
-              <Header />
-              <div className={styles.body}>
-                <aside className={styles.ignore}>
-                  <Aside />
-                </aside>
-                <article>
-                  <Suspense fallback={<Loading />}>
-                    <TheRouter />
-                  </Suspense>
-                </article>
+    <Provider store={store}>
+      <ConfigProvider locale={locale}>
+        <Router>
+          <Switch>
+            <Route path="/" exact render={() => <Redirect to={getToken() ? '/home' : '/login'} />} />
+            <Route path="/login" component={Login} />
+            <>
+              <div className={styles.layout}>
+                <Header />
+                <div className={styles.body}>
+                  <aside className={styles.ignore}>
+                    <Aside />
+                  </aside>
+                  <article>
+                    <Suspense fallback={<Loading />}>
+                      <TheRouter />
+                    </Suspense>
+                  </article>
+                </div>
               </div>
-            </div>
-          </>
-        </Switch>
-      </Router>
-    </ConfigProvider>
+            </>
+          </Switch>
+        </Router>
+      </ConfigProvider>
+    </Provider>
   )
 }
 
